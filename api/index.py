9e1dd8839a0ff9a59440675a3f10325dd3a695a3
mbron770 <mbronfin.tsd@gmail.com>
2023-10-03 23:38:50 -0400

    move

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+app = Flask(__name__)
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from database import db
@@ -7,7 +8,7 @@ import os
 
 # DB_URL = os.environ.get('DB_URL')
 DB_URL="postgresql:///default:0kfNebo1Twgp@ep-hidden-rain-16108082.us-east-1.postgres.vercel-storage.com:5432/verceldb"
-app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
