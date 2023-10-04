0547282bab224145b4dedcc4c2e5882a39a8532c
mbron770 <mbronfin.tsd@gmail.com>
2023-10-04 00:37:22 -0400

    g

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ from models import User
 import os
 
 app = Flask(__name__)
+CORS(app)
 # DB_URL = os.environ.get('DB_URL')
 DB_URL="postgresql:///default:0kfNebo1Twgp@ep-hidden-rain-16108082.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
