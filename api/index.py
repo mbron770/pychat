e28988765f31837654e98ed8d042ab2577bd5ea7
mbron770 <mbronfin.tsd@gmail.com>
2023-10-04 13:07:38 -0400

    r

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
+from http.server import BaseHTTPRequestHandler
 from database import db
 # from flask_restful import Api, Resource
 from flask_cors import CORS
@@ -17,25 +18,32 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
+    
+# @app.route("/api/python")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route('/api')
+# def hello():
+#     return "<p>Hello, World!</p>"
 
-@app.route('/api')
-def hello():
-    return "<p>Hello, World!</p>"
-
-@app.route('/api/test',  methods = ['GET', 'PATCH'])
-def test():
-    if(request.method == 'GET'):
-        all = User.query.all()
-        users = []
-        for user in all:
-            users.append(str(user))
-        return jsonify(users), 200
+# @app.route('/api/test',  methods = ['GET', 'PATCH'])
+# def test():
+#     if(request.method == 'GET'):
+#         all = User.query.all()
+#         users = []
+#         for user in all:
+#             users.append(str(user))
+#         return jsonify(users), 200
 
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        app.run()
 
